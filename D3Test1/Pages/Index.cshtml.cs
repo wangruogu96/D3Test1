bee33416930ebe56817e794c9ed38660800578c2
Ruogu Wang <ruogu@DIGITALDESK.LOCAL>
2019-09-04 16:24:23 -0500

    2019/09/04/16:24

--- D3Test1/Pages/Index.cshtml.cs
+++ D3Test1/Pages/Index.cshtml.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿
+using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
+
 namespace D3Test1.Pages
+
 {
+
     public class IndexModel : PageModel
+
     {
+
         public void OnGet()
+
         {
 
+
+
         }
+
     }
+
 }
